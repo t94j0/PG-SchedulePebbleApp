d9c99066b08084b6c40b09ea60b32c1d8c91cefd
Max Harley <t94j0@users.noreply.github.com>
2015-05-04 14:20:37 -0400

    Fix something Usefull

--- src/app.js
+++ src/app.js
@@ -1,25 +1,18 @@
 console.log('PG-School Watch App');
-simply.fullscreen(true);
+simply.fullscreen(false);
 simply.style("large");
+
 simply.on('singleClick', function(e) {
   simply.body("Loading...");
-  
-  ajax({url:"http://pg-schedule.herokuapp.com/api/timeUntil"}, function(data){
-    if(data==="-1"){
-      simply.body("Must be break, or something");
-    } else {
-      simply.vibe();
-      simply.body(data);
-    }
-  }, error());
-  
-});
-function error(e){
-  simply.text({
-    title:"Error",
-    body:"Please contact support"
+  ajax({url:"http://pg-schedule.herokuapp.com/api/timeUntil"}, function(timeUntil){
+    ajax({url:"http://pg-schedule.herokuapp.com/api/currentBlock"}, function(currentBlock){
+      simply.text({
+        subtitle:"Time \'till class ",
+        body: timeUntil + " minutes until " + currentBlock + "ends"
+      });
+    });
   });
-}
+});
 
 simply.text({
   subtitle: 'Time \'till class ',
